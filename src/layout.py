1aa26e6ed1cf864a5573720a77da5ebdebb0af6a
Iris <132371159+ihannink@users.noreply.github.com>
2024-06-17 17:18:53 +0200

    deleted corr plot, added first tooltip version

--- src/layout.py
+++ src/layout.py
@@ -1,31 +1,46 @@
 # External imports
 import dash_bootstrap_components as dbc
-from dash import html, State
+from dash import html, State, dcc
 
-# Internal imports
-from components.map_tabs import map_tabs_layout
-from components.side_nav import button, side_bar  # getAppHeader # side_navbar, Btn1
+from components.map_tabs import map_tabs_layout, choropleth_map_layout
+from components.side_nav import button, side_bar
 from components.line_graph import line_graph_layout
 from components.h_bar_chart import h_barchart_layout
-from components.map_tabs import choropleth_map_layout
 from components.correlation_graph import correlation_graph_layout
 
-# from components.scripts.data_preprocessing import geo_data, df_pas_original, df_pas_granular, df_outcomes, df_stop_search, \
-#    df_street, df_economic, df_ethnicity
-
+# Tooltip component
+tooltip = html.Div(
+    [
+        html.P(
+            [
+                "Want to know more about the selected category and the included attributes?",
+                html.Span(
+                    " Click here!",
+                    id="tooltip-target",
+                    style={"textDecoration": "underline", "cursor": "pointer"},
+                ),
+            ],
+            style={"textAlign": "center",   # Center the text
+                    "fontStyle": "italic"}   # Make the text italic
+        ),
+        dbc.Tooltip(
+            "Noun: rare, "
+            "the action or habit of estimating something as worthless.",
+            target="tooltip-target",
+        ),
+    ]
+)
 
 # ====================================
 # Create the layout
 # ====================================
 
-from dash import dcc
 dash_layout = dbc.Container([
     html.Link(href='/assets/styles.css', rel='stylesheet'),
     dcc.Store(id='stored_data', data=[]),  # Store to hold selected boroughs
     dcc.Store(id='selected_borough', data=[]),
     dcc.Store(id='shared-data-store', data=[]),
-    dcc.Store(id='shared-data-store-lg', data=[]), # Store for shared data
-
+    dcc.Store(id='shared-data-store-lg', data=[]),  # Store for shared data
 
     dbc.Row([
         dbc.Row(html.H1('Powered by the TU/e', className='top-panel')),
@@ -37,10 +52,13 @@ dash_layout = dbc.Container([
         dbc.Col(side_bar, id='sidebar-column', width=0),
         dbc.Col([
             dbc.Row([dcc.RangeSlider(2015, 2021, 1,
-                                value=[2017, 2018],
-                                id='range-slider',
-                                marks={i: str(i) for i in range(2015, 2022, 1)},
-                                ),], style={'padding': '5px'}),
+                                     value=[2017, 2018],
+                                     id='range-slider',
+                                     marks={i: str(i) for i in range(2015, 2022, 1)},
+                                     ), ], style={'padding': '5px'}),
+            dbc.Row([
+                    dbc.Col(tooltip),
+                ]),
             dbc.Row([
                 dbc.Col(choropleth_map_layout, id='map-column'),
                 dbc.Col(h_barchart_layout, id='h-barchart-column', className='graph-container'),
@@ -50,7 +68,6 @@ dash_layout = dbc.Container([
     ]),
     dbc.Row([
         dbc.Col(line_graph_layout),
-        dbc.Col(correlation_graph_layout),
     ]),
 
 ], fluid=True, className='container')
